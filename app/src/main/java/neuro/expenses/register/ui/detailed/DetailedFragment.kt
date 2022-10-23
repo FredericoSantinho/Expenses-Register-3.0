7164fb5def1f5db9ac6ce80a65a9ddec06dcbb9e
Neurophobic Animal <FredericoSantinho@gmail.com>
2022-10-23 12:01:17 +0100

    [ER-1] - Refactor.

--- app/src/main/java/neuro/expenses/register/ui/detailed/DetailedFragment.kt
+++ app/src/main/java/neuro/expenses/register/ui/detailed/DetailedFragment.kt
@@ -1,4 +1,4 @@
-package neuro.expenses.register.ui.notifications
+package neuro.expenses.register.ui.detailed
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import neuro.expenses.register.databinding.FragmentNotificationsBinding
+import neuro.expenses.register.databinding.FragmentDetailedBinding
 
-class NotificationsFragment : Fragment() {
+class DetailedFragment : Fragment() {
 
-  private var _binding: FragmentNotificationsBinding? = null
+  private var _binding: FragmentDetailedBinding? = null
 
   // This property is only valid between onCreateView and
   // onDestroyView.
@@ -22,14 +22,14 @@ class NotificationsFragment : Fragment() {
     container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View {
-    val notificationsViewModel =
-      ViewModelProvider(this).get(NotificationsViewModel::class.java)
+    val detailedViewModel =
+      ViewModelProvider(this).get(DetailedViewModel::class.java)
 
-    _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+    _binding = FragmentDetailedBinding.inflate(inflater, container, false)
     val root: View = binding.root
 
-    val textView: TextView = binding.textNotifications
-    notificationsViewModel.text.observe(viewLifecycleOwner) {
+    val textView: TextView = binding.textDetailed
+    detailedViewModel.text.observe(viewLifecycleOwner) {
       textView.text = it
     }
     return root
--- app/src/main/java/neuro/expenses/register/ui/detailed/DetailedViewModel.kt
+++ app/src/main/java/neuro/expenses/register/ui/detailed/DetailedViewModel.kt
@@ -1,13 +1,13 @@
-package neuro.expenses.register.ui.dashboard
+package neuro.expenses.register.ui.detailed
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class DetailedViewModel : ViewModel() {
 
   private val _text = MutableLiveData<String>().apply {
-    value = "This is dashboard Fragment"
+    value = "This is detailed fragment"
   }
   val text: LiveData<String> = _text
 }
