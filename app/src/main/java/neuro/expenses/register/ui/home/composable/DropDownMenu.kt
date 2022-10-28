ee4c1a820d931db62fcbf059aa40e6a5abd268f2
Neurophobic Animal <FredericoSantinho@gmail.com>
2022-10-28 09:10:11 +0100

    [ER-16] - HomeComposable.kt - Added places DropDownMenu.kt.

---
+++ app/src/main/java/neuro/expenses/register/ui/home/composable/DropDownMenu.kt
@@ -0,0 +1,65 @@
+package neuro.expenses.register.ui.home.composable
+
+import android.widget.Toast
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun DropDownMenu(
+  modifier: Modifier = Modifier,
+  color: Color = Color.Transparent,
+  label: String = "",
+  listItems: State<List<String>>
+) {
+  val contextForToast = LocalContext.current.applicationContext
+
+  var selectedItem by remember {
+    mutableStateOf(listItems.value[0])
+  }
+
+  var expanded by remember {
+    mutableStateOf(false)
+  }
+
+  ExposedDropdownMenuBox(
+    expanded = expanded,
+    onExpandedChange = {
+      expanded = !expanded
+    }, modifier = modifier
+  ) {
+    TextField(
+      value = selectedItem,
+      onValueChange = {},
+      readOnly = true,
+      label = { Text(text = label) },
+      singleLine = true,
+      trailingIcon = {
+        ExposedDropdownMenuDefaults.TrailingIcon(
+          expanded = expanded
+        )
+      },
+      colors = TextFieldDefaults.textFieldColors(
+        backgroundColor = color,
+      )
+    )
+
+    ExposedDropdownMenu(
+      expanded = expanded,
+      onDismissRequest = { expanded = false }
+    ) {
+      listItems.value.forEach { selectedOption ->
+        DropdownMenuItem(onClick = {
+          selectedItem = selectedOption
+          Toast.makeText(contextForToast, selectedOption, Toast.LENGTH_SHORT).show()
+          expanded = false
+        }) {
+          Text(text = selectedOption)
+        }
+      }
+    }
+  }
+}
--- app/src/main/java/neuro/expenses/register/ui/home/composable/HomeComposable.kt
+++ app/src/main/java/neuro/expenses/register/ui/home/composable/HomeComposable.kt
@@ -1,27 +1,35 @@
 package neuro.expenses.register.ui.home.composable
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.FragmentActivity
 import com.exchangebot.ui.theme.ExpensesRegisterTheme
 import com.google.android.gms.maps.model.LatLng
+import neuro.expenses.register.R
 import neuro.expenses.register.ui.composables.datetime.DateTimeComposable
 
 @Composable
 fun HomeComposable(fragmentActivity: FragmentActivity) {
   val latLng = remember { mutableStateOf(LatLng(37.091495, -8.2475677)) }
+  val places = remember { mutableStateOf(listOf("Bitoque")) }
 
   Column {
     MapsComposable(latLng, zoom = 19.0f)
-    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
       DateTimeComposable(fragmentActivity = fragmentActivity)
+      DropDownMenu(
+        modifier = Modifier
+          .padding(start = 8.dp)
+          .requiredWidth(180.dp),
+        label = stringResource(id = R.string.home_place),
+        listItems = places
+      )
     }
   }
 }
