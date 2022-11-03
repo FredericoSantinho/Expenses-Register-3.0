6c237a9288764bcbf6c00e7927a488f513c26333
Neurophobic Animal <FredericoSantinho@gmail.com>
2022-11-03 12:22:00 +0000

    [ER-5] - DateTextMapperImpl.kt - Integrated NumberFormater.

--- app/src/main/java/neuro/expenses/register/ui/composables/datetime/mapper/DateTextMapperImpl.kt
+++ app/src/main/java/neuro/expenses/register/ui/composables/datetime/mapper/DateTextMapperImpl.kt
@@ -1,7 +1,11 @@
 package neuro.expenses.register.ui.composables.datetime.mapper
 
-class DateTextMapperImpl : DateTextMapper {
+import neuro.expenses.register.common.formatter.NumberFormater
+import neuro.expenses.register.common.formatter.NumberFormaterImpl
+
+class DateTextMapperImpl(private val numberFormater: NumberFormater = NumberFormaterImpl()) :
+  DateTextMapper {
   override fun map(day: Int, month: Int, year: Int): String {
-    return day.toString() + '/' + month + '/' + year
+    return numberFormater.format(day) + '/' + numberFormater.format(month) + '/' + year
   }
 }
