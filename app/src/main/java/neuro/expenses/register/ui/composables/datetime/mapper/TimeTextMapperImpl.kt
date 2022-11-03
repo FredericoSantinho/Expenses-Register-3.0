4023a3bfa8bcd94e6b68e506c00fb97ee88a8af1
Neurophobic Animal <FredericoSantinho@gmail.com>
2022-11-03 12:22:28 +0000

    [ER-8] - TimeTextMapperImpl.kt - Integrated NumberFormater.

--- app/src/main/java/neuro/expenses/register/ui/composables/datetime/mapper/TimeTextMapperImpl.kt
+++ app/src/main/java/neuro/expenses/register/ui/composables/datetime/mapper/TimeTextMapperImpl.kt
@@ -1,7 +1,11 @@
 package neuro.expenses.register.ui.composables.datetime.mapper
 
-class TimeTextMapperImpl : TimeTextMapper {
+import neuro.expenses.register.common.formatter.NumberFormater
+import neuro.expenses.register.common.formatter.NumberFormaterImpl
+
+class TimeTextMapperImpl(private val numberFormater: NumberFormater = NumberFormaterImpl()) :
+  TimeTextMapper {
   override fun map(hour: Int, minute: Int): String {
-    return hour.toString() + 'h' + minute.toString()
+    return numberFormater.format(hour) + 'h' + numberFormater.format(minute)
   }
 }
