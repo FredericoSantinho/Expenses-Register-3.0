a18628bdf34dc0af2bc912c9ab5acb837adacb1d
Neurophobic Animal <FredericoSantinho@gmail.com>
2022-11-07 10:52:09 +0000

    [ER-2] - Added permission request before accessing the app.

--- app/src/main/java/neuro/expenses/register/MainActivity.kt
+++ app/src/main/java/neuro/expenses/register/MainActivity.kt
@@ -1,8 +1,14 @@
 package neuro.expenses.register
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.WindowManager
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.view.forEach
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -11,6 +17,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import neuro.expenses.register.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+  private val PERMISSIONS_REQUEST_CODE = 1
 
   private lateinit var binding: ActivityMainBinding
 
@@ -27,8 +34,6 @@ class MainActivity : AppCompatActivity() {
     val navView: BottomNavigationView = binding.navView
 
     val navController = findNavController(R.id.nav_host_fragment_activity_main)
-    // Passing each menu ID as a set of Ids because each
-    // menu should be considered as top level destinations.
     val appBarConfiguration = AppBarConfiguration(
       setOf(
         R.id.navigation_home,
@@ -39,5 +44,72 @@ class MainActivity : AppCompatActivity() {
     )
     setupActionBarWithNavController(navController, appBarConfiguration)
     navView.setupWithNavController(navController)
+
+    disableBottomNavigation()
+    checkPermissions(this)
+  }
+
+  private fun checkPermissions(activity: AppCompatActivity) {
+    if (ContextCompat.checkSelfPermission(
+        activity,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+      ) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+        activity,
+        Manifest.permission.ACCESS_FINE_LOCATION
+      ) != PackageManager.PERMISSION_GRANTED
+    ) {
+      showPermissionsDialog()
+    } else {
+      enableBottomNavigation()
+    }
+  }
+
+  private fun showPermissionsDialog() {
+    AlertDialog.Builder(this)
+      .setTitle(R.string.permissions_title)
+      .setMessage(R.string.permissions_text)
+      .setPositiveButton(android.R.string.ok, { dialog, which ->
+        requestPermissions(this)
+      })
+      .setNegativeButton(android.R.string.cancel, { dialog, which ->
+        finish()
+      })
+      .setIcon(android.R.drawable.ic_dialog_alert)
+      .show()
+  }
+
+  private fun requestPermissions(activity: AppCompatActivity) {
+    ActivityCompat.requestPermissions(
+      activity,
+      arrayOf(
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION
+      ),
+      PERMISSIONS_REQUEST_CODE
+    )
+  }
+
+  private fun enableBottomNavigation() {
+    setNavViewEnabled(true)
+  }
+
+  private fun disableBottomNavigation() {
+    setNavViewEnabled(false)
+  }
+
+  private fun setNavViewEnabled(enabled: Boolean) {
+    binding.navView.menu.forEach { it.isEnabled = enabled }
+  }
+
+  override fun onRequestPermissionsResult(
+    requestCode: Int,
+    permissions: Array<out String>,
+    grantResults: IntArray
+  ) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+    if (requestCode == PERMISSIONS_REQUEST_CODE) {
+      checkPermissions(this)
+    }
   }
 }
---
+++ app/src/main/java/neuro/expenses/register/ui/splash/view/model/SplashViewModel.kt
@@ -0,0 +1,5 @@
+package neuro.expenses.register.ui.home.view.model
+
+import androidx.lifecycle.ViewModel
+
+class SplashViewModel : ViewModel()
