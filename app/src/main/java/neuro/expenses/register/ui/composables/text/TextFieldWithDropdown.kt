7c375ed6377a4e5db7dcbf2c668ffe864b368a3a
Neurophobic Animal <FredericoSantinho@gmail.com>
2022-11-07 20:33:34 +0000

    [ER-40] - TextFieldWithDropdown.kt now moves focus to next when selection is made.

--- app/src/main/java/neuro/expenses/register/ui/composables/text/TextFieldWithDropdown.kt
+++ app/src/main/java/neuro/expenses/register/ui/composables/text/TextFieldWithDropdown.kt
@@ -7,10 +7,11 @@ import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextRange
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.window.PopupProperties
 import com.exchangebot.ui.theme.ExpensesRegisterTheme
@@ -29,54 +30,49 @@ fun TextFieldWithDropdown(
 ) {
 
   val dropDownOptions = remember { mutableStateOf(listOf<String>()) }
-  val textFieldValue = remember { mutableStateOf(TextFieldValue(value.value)) }
   val dropDownExpanded = remember { mutableStateOf(false) }
 
   fun onDropdownDismissRequest() {
     dropDownExpanded.value = false
   }
 
-  fun onValueChanged(value: TextFieldValue) {
-    dropDownExpanded.value = value.text.isNotBlank()
-    textFieldValue.value = value
+  fun onValueChanged(value: String) {
+    dropDownExpanded.value = value.isNotBlank()
     dropDownOptions.value = dataIn.value.filter {
-      it.startsWith(value.text) && it != value.text
+      it.startsWith(value) && it != value
     }.take(take)
-    onValueChange.invoke(value.text)
+    onValueChange.invoke(value)
   }
 
+  val focusManager = LocalFocusManager.current
   TextFieldWithDropdown(
     modifier = modifier,
-    textFieldValue,
-    setValue = ::onValueChanged,
+    setValue = { onValueChanged(it) },
     onDismissRequest = ::onDropdownDismissRequest,
     dropDownExpanded = dropDownExpanded.value,
     list = dropDownOptions.value,
     label = label,
     keyboardOptions,
     value,
-    isError
+    isError,
+    focusManager
   )
 }
 
 @Composable
 fun TextFieldWithDropdown(
   modifier: Modifier = Modifier,
-  textFieldValue: MutableState<TextFieldValue>,
-  setValue: (TextFieldValue) -> Unit,
+  setValue: (String) -> Unit,
   onDismissRequest: () -> Unit,
   dropDownExpanded: Boolean,
   list: List<String>,
   label: String = "",
   keyboardOptions: KeyboardOptions,
   value: MutableState<String>,
-  isError: MutableState<Boolean>
+  isError: MutableState<Boolean>,
+  focusManager: FocusManager
 ) {
   var isErrorVar by rememberSaveable { isError }
-
-  if (value.value == "") {
-    setValue.invoke(TextFieldValue(value.value))
-  }
 
   Box(modifier) {
     TextField(
@@ -87,10 +83,10 @@ fun TextFieldWithDropdown(
             onDismissRequest()
         },
       keyboardOptions = keyboardOptions,
-      value = textFieldValue.value,
+      value = value.value,
       onValueChange = {
         isErrorVar = false
-        value.value = it.text
+        value.value = it
         setValue.invoke(it)
       },
       label = { Text(label) },
@@ -110,12 +106,8 @@ fun TextFieldWithDropdown(
       list.forEach { text ->
         DropdownMenuItem(onClick = {
           value.value = text
-          setValue(
-            TextFieldValue(
-              value.value,
-              TextRange(value.value.length)
-            )
-          )
+          setValue(value.value)
+          focusManager.moveFocus(FocusDirection.Next)
         }) {
           Text(text = text)
         }
