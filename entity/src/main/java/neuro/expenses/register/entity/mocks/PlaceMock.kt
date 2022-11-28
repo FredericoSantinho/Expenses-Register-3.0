55d14950b531327efc6f1e9edabb07be9ec2f31d
Neurophobic Animal <FredericoSantinho@gmail.com>
2022-11-28 18:31:19 +0000

    [ER-109] - Crates GetOrCreatePlaceImplTest.kt.

--- entity/src/main/java/neuro/expenses/register/entity/mocks/PlaceMock.kt
+++ entity/src/main/java/neuro/expenses/register/entity/mocks/PlaceMock.kt
@@ -1,7 +1,12 @@
 package neuro.expenses.register.entity.mocks
 
 import neuro.expenses.register.entity.Place
+import neuro.expenses.register.entity.PlaceProduct
 
-fun placeMock(id: Long = 1L): Place {
-  return Place(id, "name$id", placeProductsMock(), latLngMock())
+fun placeMock(
+  id: Long = 1L,
+  placeName: String = "name$id",
+  placeProducts: List<PlaceProduct> = placeProductsMock()
+): Place {
+  return Place(id, placeName, placeProducts, latLngMock())
 }
