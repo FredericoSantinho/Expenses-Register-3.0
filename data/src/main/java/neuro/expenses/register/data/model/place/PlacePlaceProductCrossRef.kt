8450ff64cf2770c6770c441bbf10ab5e4eb2e76d
Neurophobic Animal <FredericoSantinho@gmail.com>
2022-11-29 00:56:18 +0000

    [ER-109] - PlacePlaceProductCrossRef.kt - Added foreignKeys.

--- data/src/main/java/neuro/expenses/register/data/model/place/PlacePlaceProductCrossRef.kt
+++ data/src/main/java/neuro/expenses/register/data/model/place/PlacePlaceProductCrossRef.kt
@@ -1,9 +1,25 @@
 package neuro.expenses.register.data.model.place
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.Index
+import neuro.expenses.register.data.model.RoomPlaceProduct
 
-@Entity(primaryKeys = ["placeId", "placeProductId"], indices = [Index(value = ["placeProductId"])])
+@Entity(
+  primaryKeys = ["placeId", "placeProductId"],
+  indices = [Index(value = ["placeProductId"])],
+  foreignKeys = [ForeignKey(
+    entity = RoomPlace::class,
+    parentColumns = arrayOf("placeId"),
+    childColumns = arrayOf("placeId"),
+    onDelete = ForeignKey.RESTRICT
+  ), ForeignKey(
+    entity = RoomPlaceProduct::class,
+    parentColumns = arrayOf("placeProductId"),
+    childColumns = arrayOf("placeProductId"),
+    onDelete = ForeignKey.RESTRICT
+  )]
+)
 data class PlacePlaceProductCrossRef(
   val placeId: Long,
   val placeProductId: Long
